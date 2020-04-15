8b4d2914d919b2b8a6275913755267da9401aca8
alexdzius <akeldwithaviation@gmail.com>
2020-04-15 14:08:12 -0700

    wefewf

---
+++ TheGame/New Unity Project/Assets/Scripts/aProjectiveMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aProjectiveMove : MonoBehaviour
+{
+    public Rigidbody2D aMovObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aMovObject = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aShooting.aNegative) 
+        {
+            aMovObject.transform.Translate(Vector2.left * -5 * Time.deltaTime);
+        } 
+        else
+        {
+            aMovObject.transform.Translate(Vector2.left * 5 * Time.deltaTime);
+        }
+    }
+}
