5bbf6d83e89c4380e4395a266cb11258d652490e
alexdzius <akeldwithaviation@gmail.com>
2020-04-15 15:01:38 -0700

    fixed

---
+++ TheGame/New Unity Project/Assets/Scripts/aPlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aPlayerController : MonoBehaviour
+{
+    public GameObject[] aProjectile;
+    public Rigidbody2D amyRB;
+    public static bool aNegative = false;
+    public static int aScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        amyRB = GetComponent<Rigidbody2D>();
+        amyRB.velocity = new Vector3(0, -2, 0);
+        aScore = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("left") || Input.GetKeyDown("a")) 
+        {
+            Instantiate(aProjectile[0], new Vector3(transform.position.x -1, transform.position.y, transform.position.z), Quaternion.identity);
+            aNegative = true;
+        }
+        if(Input.GetKeyDown("right") || Input.GetKeyDown("d"))
+        {
+            Instantiate(aProjectile[1], new Vector3(transform.position.x + 1, transform.position.y, transform.position.z), Quaternion.identity);
+            aNegative = false;
+        }
+        if(aScore == 3 && amyRB.transform.position.y == -6)
+        {
+            print("u won B");
+            // proceed to next level
+        }
+    }
+}
--- TheGame/New Unity Project/Assets/Scripts/aProjectiveMove.cs
+++ TheGame/New Unity Project/Assets/Scripts/aProjectiveMove.cs
@@ -5,22 +5,47 @@ using UnityEngine;
 public class aProjectiveMove : MonoBehaviour
 {
     public Rigidbody2D aMovObject;
+    public bool aMinus;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // if it hits scoreing tile
+        if(collision.gameObject.tag == "Finish")
+        {
+            aPlayerController.aScore++;
+            Destroy(gameObject);
+        }
+        // if it hits platform
+        else if(collision.gameObject.tag == "EditorOnly")
+        {
+            Destroy(gameObject);
+        }
+        else 
+        {
+            // do nothing lol
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
         aMovObject = GetComponent<Rigidbody2D>();
+        aMinus = aPlayerController.aNegative;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (aShooting.aNegative) 
-        {
-            aMovObject.transform.Translate(Vector2.left * -5 * Time.deltaTime);
-        } 
-        else
+        if (aMinus) 
         {
             aMovObject.transform.Translate(Vector2.left * 5 * Time.deltaTime);
+        } 
+        else 
+        {
+            aMovObject.transform.Translate(Vector2.right * 5 * Time.deltaTime);
+        }
+
+        if(aMovObject.transform.position.x > 9 || aMovObject.transform.position.x < -9)
+        {
+            Destroy(gameObject);
         }
     }
 }
