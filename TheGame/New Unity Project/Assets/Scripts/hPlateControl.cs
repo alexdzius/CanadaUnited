4a3b175eb4236bd2f5cdb89571d2da209b286a1c
hjenningsramirez123 <h.jenningsramirez@digipen.edu>
2020-04-16 15:04:40 -0700

    Added movement for my plate and updated my scene. P.S. we should get some art soon.

---
+++ TheGame/New Unity Project/Assets/Scripts/hPlateControl.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * By: Harry Jennings-Ramirez
+ * Last Edited: 4/16/2020
+ * Description: Moves the plate only on the x-axis
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hPlateControl : MonoBehaviour
+{
+    // Adjust the speed of the plate
+    public float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Gets the input to move plate
+        if (Input.GetKey("left") || Input.GetKey("a"))
+        {
+            transform.position = transform.position + new Vector3(-speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("right") || Input.GetKey("d"))
+        {
+            transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
