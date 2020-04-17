7c42e622d35342472796c80128f94a42d4c9d356
hjenningsramirez123 <h.jenningsramirez@digipen.edu>
2020-04-17 15:20:34 -0700

    Added background sprite, updated my scene and burger.prefab.

--- TheGame/New Unity Project/Assets/Scripts/hFallingFood.cs
+++ TheGame/New Unity Project/Assets/Scripts/hFallingFood.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * By: Harry Jennings-Ramirez
- * Last Edited: 4/15/2020
+ * Last Edited: 4/17/2020
  * Description: Spawns a falling object (burger) randomly on the x-axis
  */
 using System.Collections;
@@ -9,30 +9,22 @@ using UnityEngine;
 
 public class hFallingFood : MonoBehaviour
 {
-    private GameObject Burger;
+    public GameObject Burger;
 
     // Makes sure it spawns within the wanted range
     public float xMin;
     public float xMax;
-
-    // Used to make sure it only spawns once
-    private bool burgerSpawned;
+    public float yMax;
 
     // Start is called before the first frame update
     void Start()
     {
-        burgerSpawned = false;
+        GameObject.Instantiate(Burger, new Vector2(Random.Range(xMin, xMax), yMax), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Ensures it only spawns once
-        Burger = GameObject.Find("burger");
-        if (!burgerSpawned)
-        {
-            burgerSpawned = true;
-            Instantiate(Burger, new Vector2(Random.Range(xMin, xMax), 5), Quaternion.identity);
-        }
+
     }
 }
