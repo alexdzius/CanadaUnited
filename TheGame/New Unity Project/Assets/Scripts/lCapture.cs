b1d2f966a07f6a36592914b5f06ab16364c1b1d7
bluephaser <liam.binford@digipen.edu>
2020-04-17 14:50:16 -0700

    collision stuff

---
+++ TheGame/New Unity Project/Assets/Scripts/lCapture.cs
@@ -0,0 +1,42 @@
+﻿/*
+ lCapture.cs
+ By Liam Binford
+ 4/17/20
+ This script allows the player to hover the reticle over the target and press a button to capture it
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lCapture : MonoBehaviour
+{
+    public GameObject target;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            print("touch");
+            if (Input.GetKey("space"))
+            {
+                GameManager.aTotalScore += 3;
+                GameManager.aAllowedChange = true;
+                GameManager.aLevel = (GameManager.aCurrentLevel)Random.Range(0, 5);
+            }
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
